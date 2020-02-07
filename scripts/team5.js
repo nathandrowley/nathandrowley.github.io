7f036d59661c71a27a825bf31916a8c466801a32
nathan rowley <row16016@byui.edu>
2020-02-07 11:11:22 -0800

    Week 5 additions

---
+++ scripts/team5.js
@@ -0,0 +1,10 @@
+  function detailReport() {
+      let report = document.getElementById("bechlerFalls");
+      if (report.style.display ==="none") {
+          report.style.display = "block";
+      }
+      else {
+          report.style.display = "none";
+      }
+
+  }
