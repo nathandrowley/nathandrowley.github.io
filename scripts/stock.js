3779254e89d6b89aad851ecfdd7307b035e49f15
Nathan Rowley <nathandrowley@gmail.com>
2020-04-04 17:11:06 -0700

    final changes, still api errors

--- scripts/stock.js
+++ scripts/stock.js
@@ -1,31 +1,25 @@
 
+
+
 function search() {
 var stock = document.getElementById("userinput").value;
 
+
 fetch("https://financialmodelingprep.com/api/v3/quote/" + stock)
 .then(res => res.json())
-.then(data => {generateHTML(data)}
+.then(data => {generateHTML(data)})
 
-)}
-
-// const newLocal = JSON.stringify(data);
+};
 
 const generateHTML = (data) => {
     console.log(data)
-    const html = 
-   `${data}`
-  
-    // <div class="name">${data.name}</div>
-    // <div class="currency">${data.currency}</div>
-    // <div class="stockExchange">${data.stockExchange}</div>
-    // <div class="exchangeShortName">${data.exchangeShortName}</div>
+//     const myStock = data;
+//     const displayStock = JSON.parse(myStock);
     
-   
-const stockCon = document.querySelector('.stockContainer')
-stockCon.innerHTML = html
-console.log(html)
-}
+//     document.getElementById("symbol").innerHTML = displayStock;
 
-var obj = {prop1: 'prop1Value', prop2: 'prop2Value', child: {childProp1: 'childProp1Value'}}
-    console.log(obj)
+// var symbol = data.Array.symbol;
+
+// $('.symbol').append(symbol);
+}
 
